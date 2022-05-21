5d4876c4e5fd99c8b9a7590f2b2adebe4a1b4553
kkawalec1 <92663431+kkawalec1@users.noreply.github.com>
2022-05-21 13:08:18 +0200

    Add files via upload

---
+++ AplikacjaController.java
@@ -0,0 +1,24 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+import java.security.cert.PolicyNode;
+
+public class AplikacjaController {
+    @FXML
+    private TextField imie;
+    @FXML
+    private Button Zatwierdz;
+    @FXML
+    private TextField nazwisko;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField haslo;
+    @FXML
+    private TextField phaslo;
+    @FXML
+    private Label komunikat;
+
+}
